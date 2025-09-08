bde6c61c79865efc85041f0c240d6b851166d07a
leiwingqueen <leiwingqueen@gmail.com>
2025-09-09 00:41:57 +0800

    代码修改

--- src/main/java/com/example/flink/WordCount.java
+++ src/main/java/com/example/flink/WordCount.java
@@ -1,11 +1,9 @@
 package com.example.flink;//package p1;
 
-import org.apache.flink.api.common.ExecutionConfig;
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
-import org.apache.flink.api.java.operators.UnsortedGrouping;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.ParameterTool;
 
@@ -20,11 +18,7 @@ public class WordCount {
 
         DataSet<String> text = env.readTextFile(params.get("input"));
 
-        DataSet<String> filtered = text.filter(new FilterFunction<String>() {
-            public boolean filter(String value) {
-                return value.startsWith("N");
-            }
-        });
+        DataSet<String> filtered = text.filter((FilterFunction<String>) value -> value.startsWith("N"));
         DataSet<Tuple2<String, Integer>> tokenized = filtered.map(new Tokenizer());
 
         DataSet<Tuple2<String, Integer>> counts = tokenized.groupBy(new int[]{
