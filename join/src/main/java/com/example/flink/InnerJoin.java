78d58ea420c350678cadbc16063aa01b483b26bc
leiwingqueen <leiwingqueen@gmail.com>
2025-09-14 18:26:23 +0800

    remove some code

--- join/src/main/java/com/example/flink/InnerJoin.java
+++ join/src/main/java/com/example/flink/InnerJoin.java
@@ -4,6 +4,7 @@ import org.apache.flink.api.common.functions.JoinFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.DataSource;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.api.java.utils.ParameterTool;
@@ -19,33 +20,22 @@ public class InnerJoin {
         // make parameters available in the web interface
         env.getConfig().setGlobalJobParameters(params);
 
+        DataSource<String> ds1 = env.readTextFile(params.get("input1"));
+        DataSource<String> ds2 = env.readTextFile(params.get("input2"));
+        // hint:
+        // input1 = path to person.csv
+        // input2 = path to location.csv
         // Read person file and generate tuples out of each string read
-        DataSet<Tuple2<Integer, String>> personSet = env.readTextFile(params.get("input1"))
-                .map(new MapFunction<String, Tuple2<Integer, String>>()                                     //presonSet = tuple of (1  John)
-                {
-                    public Tuple2<Integer, String> map(String value) {
-                        String[] words = value.split(",");                                                 // words = [ {1} {John}]
-                        return new Tuple2<Integer, String>(Integer.parseInt(words[0]), words[1]);
-                    }
-                });
-        // Read location file and generate tuples out of each string read
-        DataSet<Tuple2<Integer, String>> locationSet = env.readTextFile(params.get("input2")).
-                map(new MapFunction<String, Tuple2<Integer, String>>() {                                                                                                 //locationSet = tuple of (1  DC)
-                    public Tuple2<Integer, String> map(String value) {
-                        String[] words = value.split(",");
-                        return new Tuple2<Integer, String>(Integer.parseInt(words[0]), words[1]);
-                    }
-                });
+        // person file format: person_id, person_name
+        // location file format: person_id, state
+        // use join,where,equalTo,with function to join two dataset
+        // output person_id,person_name,state
+        // TODO: implement here
+
 
         // join datasets on person_id
         // joined format will be <id, person_name, state>
-        DataSet<Tuple3<Integer, String, String>> joined = personSet.join(locationSet).where(0).equalTo(0)
-                .with(new JoinFunction<Tuple2<Integer, String>, Tuple2<Integer, String>, Tuple3<Integer, String, String>>() {
-
-                    public Tuple3<Integer, String, String> join(Tuple2<Integer, String> person, Tuple2<Integer, String> location) {
-                        return new Tuple3<Integer, String, String>(person.f0, person.f1, location.f1);         // returns tuple of (1 John DC)
-                    }
-                });
+        DataSet<Tuple3<Integer, String, String>> joined;
 
         joined.writeAsCsv(params.get("output"), "\n", " ");
 
