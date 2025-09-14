177fe1bf744e7d9d4556713e60aa64fce9de69eb
leiwingqueen <leiwingqueen@gmail.com>
2025-09-14 20:30:11 +0800

    implement left join,right join and outer join

---
+++ join/src/main/java/com/example/flink/OuterJoin.java
@@ -0,0 +1,70 @@
+package com.example.flink;
+
+import org.apache.flink.api.common.functions.JoinFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.api.java.operators.MapOperator;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.api.java.utils.ParameterTool;
+
+@SuppressWarnings("serial")
+public class OuterJoin {
+    public static void main(String[] args) throws Exception {
+
+        // set up the execution environment
+        final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+        final ParameterTool params = ParameterTool.fromArgs(args);
+        // make parameters available in the web interface
+        env.getConfig().setGlobalJobParameters(params);
+        // 设置并行度
+        if (params.has("parallelism")){
+            env.setParallelism(params.getInt("parallelism"));
+        }
+        DataSource<String> ds1 = env.readTextFile(params.get("input1"));
+        DataSource<String> ds2 = env.readTextFile(params.get("input2"));
+        // hint:
+        // input1 = path to person.csv
+        // input2 = path to location.csv
+        // Read person file and generate tuples out of each string read
+        // person file format: person_id, person_name
+        // location file format: person_id, state
+        // use join,where,equalTo,with function to join two dataset
+        // output person_id,person_name,state
+        // implement here
+        MapOperator<String, Tuple2<Integer, String>> mapOp1 = ds1.map((MapFunction<String, Tuple2<Integer, String>>) s -> {
+            String[] arr = s.split(",");
+            if (arr.length < 2) {
+                throw new IllegalArgumentException("param error");
+            }
+            return new Tuple2<>(Integer.parseInt(arr[0]), arr[1]);
+        }).returns(Types.TUPLE(Types.INT, Types.STRING));
+        MapOperator<String, Tuple2<Integer, String>> mapOp2 = ds2.map((MapFunction<String, Tuple2<Integer, String>>) s -> {
+            String[] arr = s.split(",");
+            if (arr.length < 2) {
+                throw new IllegalArgumentException("param error");
+            }
+            return new Tuple2<>(Integer.parseInt(arr[0]), arr[1]);
+        }).returns(Types.TUPLE(Types.INT, Types.STRING));
+        DataSet<Tuple3<Integer, String, String>> joined = mapOp1.fullOuterJoin(mapOp2).where(0).equalTo(0).
+                with((JoinFunction<Tuple2<Integer, String>, Tuple2<Integer, String>, Tuple3<Integer, String, String>>) (p1, p2) ->{
+            if (p1 == null) {
+                return new Tuple3<>(p2.f0, "null", p2.f1);
+            }else if (p2 == null) {
+                return new Tuple3<>(p1.f0, p1.f1, "null");
+            }else{
+                return new Tuple3<>(p1.f0, p1.f1, p2.f1);
+            }
+        }).returns(Types.TUPLE(Types.INT, Types.STRING, Types.STRING));
+        // join datasets on person_id
+        // joined format will be <id, person_name, state>
+
+        joined.writeAsCsv(params.get("output"), "\n", " ");
+
+        env.execute("Join example");
+    }
+}
